6bac47e55ea6385a9ae5701006e074a111653dc8
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2026-06-09 15:42:51 -0400

    Completed IncidentAnalyzer

--- src/main/java/edu/touro/las/mcon364/final_test/IncidentAnalyzer.java
+++ src/main/java/edu/touro/las/mcon364/final_test/IncidentAnalyzer.java
@@ -5,6 +5,7 @@ import edu.touro.las.mcon364.final_test.SupportTicket;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -28,7 +29,7 @@ import java.util.stream.Collectors;
  */
 public class IncidentAnalyzer {
     //TODO - uncomment this field and initialize it in the constructor to store the incidents passed in.
-    //private final List<SupportTicket> incidents;
+    private final List<SupportTicket> incidents;
 
     /**
      * Store the incidents that this analyzer will examine.
@@ -37,6 +38,10 @@ public class IncidentAnalyzer {
      */
     public IncidentAnalyzer(List<SupportTicket> incidents) {
        //TODO - implement this constructor
+        if(incidents==null){
+            throw new NullPointerException("Incidents cannot be null");
+        }
+        this.incidents = List.copyOf(incidents);
     }
 
     /**
@@ -44,7 +49,7 @@ public class IncidentAnalyzer {
      */
     public long getClosedCount() {
         //TODO - implement this method
-        return -1;
+        return  incidents.stream().filter(SupportTicket::resolved).count();
     }
 
     /**
@@ -54,7 +59,8 @@ public class IncidentAnalyzer {
      */
     public double getAverageTimeToClose() {
         //TODO - implement this method
-        return 0.0;
+        return incidents.stream().filter(SupportTicket::resolved)
+                .mapToDouble(SupportTicket::minutesToResolve).average().orElse(0.0);
     }
 
     /**
@@ -62,7 +68,12 @@ public class IncidentAnalyzer {
      */
     public Map<String, Long> getCountByCategory() {
         //TODO - implement this method
-        return null;
+        return Map.copyOf(incidents.stream().collect(
+                Collectors.groupingBy(
+                        SupportTicket::category,
+                        Collectors.counting()
+                )
+        ));
     }
 
     /**
@@ -70,6 +81,6 @@ public class IncidentAnalyzer {
      */
     public List<SupportTicket> getCriticalOpenIncidents() {
         //TODO - implement this method
-        return null;
+        return List.copyOf(incidents.stream().filter(t-> !t.resolved()&&t.priority() == Priority.HIGH).toList());
     }
 }
